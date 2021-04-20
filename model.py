51e4325d2d4327fc0dc9b6015425bb58ec28e8b0
Tsao-Lun, Chen <31922920+wahahahaya@users.noreply.github.com>
2021-04-20 17:29:15 +0800

    Add files via upload

--- model.py
+++ model.py
@@ -42,28 +42,12 @@ class Up(nn.Module):
 
     def __init__(self, in_channels, out_channels, bilinear=True):
         super().__init__()
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv = DoubleConv(in_channels, out_channels)
 
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels , in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
-
-    def forward(self, x1, x2):
-        x1 = self.up(x1)
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
-        # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-        x = torch.cat([x2, x1], dim=1)
-        return self.conv(x)
+    def forward(self, x):
+        x1 = self.up(x)
+        return self.conv(x1)
 
 
 class OutConv(nn.Module):
@@ -76,19 +60,14 @@ class OutConv(nn.Module):
 
 
 class Encoder(nn.Module):
-    def __init__(self, bilinear=True):
+    def __init__(self):
         super(Encoder, self).__init__()
-        self.n_channels = 1
-        self.n_classes = 1
-        self.bilinear = bilinear
-
         self.inc = DoubleConv(1, 64)
         self.down1 = Down(64, 128)
         self.down2 = Down(128, 256)
         self.down3 = Down(256, 512)
         self.down4 = Down(512, 512)
         self.down5 = Down(512, 512)
-
 
     def forward(self, x):
         x1 = self.inc(x)
@@ -97,15 +76,71 @@ class Encoder(nn.Module):
         x4 = self.down3(x3)
         x5 = self.down4(x4)
         x6 = self.down5(x5)
-
         embedding = x6.view(-1, 512 * 8 * 8)
-        return embedding
 
-class Decoder(nn.torch):
+        return embedding, x6
+
+class Decoder_denoise(nn.Module):
     def __init__(self):
-        super(Decoder, self).__init__()
+        super(Decoder_denoise, self).__init__()
+        self.up1 = Up(512, 512)
+        self.up2 = Up(512, 512)
+        self.up3 = Up(512, 256)
+        self.up4 = Up(256, 128)
+        self.up5 = Up(128, 64)
+        self.outc = OutConv(64, 1)
 
-        
+    def forward(self, x):
+        x1 = self.up1(x)
+        x2 = self.up2(x1)
+        x3 = self.up3(x2)
+        x4 = self.up4(x3)
+        x5 = self.up5(x4)
+        logits = self.outc(x5)
+
+        return logits
+
+
+class Decoder_SP(nn.Module):
+    def __init__(self):
+        super(Decoder_SP, self).__init__()
+        self.up1 = Up(512, 512)
+        self.up2 = Up(512, 512)
+        self.up3 = Up(512, 256)
+        self.up4 = Up(256, 128)
+        self.up5 = Up(128, 64)
+        self.outc = OutConv(64, 1)
+
+    def forward(self, x):
+        x1 = self.up1(x)
+        x2 = self.up2(x1)
+        x3 = self.up3(x2)
+        x4 = self.up4(x3)
+        x5 = self.up5(x4)
+        logits = self.outc(x5)
+
+        return logits
+
+
+class Decoder_AWGN(nn.Module):
+    def __init__(self):
+        super(Decoder_AWGN, self).__init__()
+        self.up1 = Up(512, 512)
+        self.up2 = Up(512, 512)
+        self.up3 = Up(512, 256)
+        self.up4 = Up(256, 128)
+        self.up5 = Up(128, 64)
+        self.outc = OutConv(64, 1)
+
+    def forward(self, x):
+        x1 = self.up1(x)
+        x2 = self.up2(x1)
+        x3 = self.up3(x2)
+        x4 = self.up4(x3)
+        x5 = self.up5(x4)
+        logits = self.outc(x5)
+
+        return logits
 
 
 class Dis(nn.Module):
@@ -234,4 +269,5 @@ if __name__ == "__main__":
    device = "cuda" if torch.cuda.is_available() else "cpu"
    x = torch.randn(1 ,1, 256, 256).to(device)
    model = Encoder().to(device)
-   print(model(x).shape)
+   dec = Decoder_AWGN().to(device)
+   print(dec(model(x)[1]).shape)
